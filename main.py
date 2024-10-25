1ae293c418f2e316af001c2047c24afd5e8a341f
estarace <30480936+estarace@users.noreply.github.com>
2024-10-24 21:40:56 -0500

    Update main.py with comments

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#Update in the future to take an argument from command line, or from a menu selection
 book="./books/frankenstein.txt"
 
-
+# Function to count the words in a given book or text. Words are sets of characters between whitespace
 def wordCount(bkData):
     words=bkData.split()
     return(len(words))
 
+# Function to count the number of alphabetic characters in a given book or text
 def charCount(bkData):
     alpha={}
     for i in bkData:
@@ -16,7 +18,7 @@ def charCount(bkData):
                 alpha[x]=1
     
     return(alpha)
-
+#In the future, wrap the report below into a function
 with open(book,"r") as f:
     data=f.read()
     print(f"--- Begin report of books/frankenstein.txt ---")
